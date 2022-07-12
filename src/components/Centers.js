579467e0b342b49b86f5444d95504d661cdb6351
MMostella <masonmostella@gmail.com>
2022-07-11 20:45:15 -0500

    Created empty Components

---
+++ src/components/Centers.js
@@ -0,0 +1,7 @@
+export default function Centers() {
+  return (
+    <div>
+      <h1>Weight Loss Centers</h1>
+    </div>
+  );
+}
---
+++ src/components/Why.js
@@ -0,0 +1,7 @@
+export default function Why() {
+  return (
+    <div>
+      <h1>Why B2B</h1>
+    </div>
+  );
+}
