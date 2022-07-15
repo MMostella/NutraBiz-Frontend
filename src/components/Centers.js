289a32c27617042849f86095b6ffc59dfeda2f79
MMostella <masonmostella@gmail.com>
2022-07-15 15:52:04 -0500

    Linked Login and Register in Centers

--- src/components/Centers.js
+++ src/components/Centers.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import styled from "styled-components";
 
 const StyledCenters = styled.div`
@@ -8,10 +10,29 @@ const StyledCenters = styled.div`
   height: 100vh;
 `;
 
+const linkStyle = {
+  color: "black",
+  // textDecoration: "none",
+};
+
 export default function Centers() {
   return (
     <StyledCenters>
       <h1>Weight Loss Centers</h1>
+      <h3>
+        Register to become a Weight Loss Center! Click{" "}
+        <Link to="/register" style={linkStyle}>
+          {" "}
+          HERE
+        </Link>
+      </h3>
+      <h3>
+        Already a Center? Click{" "}
+        <Link to="/login" style={linkStyle}>
+          {" "}
+          HERE
+        </Link>
+      </h3>
     </StyledCenters>
   );
 }
