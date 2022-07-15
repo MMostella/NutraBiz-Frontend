eff69d2a0568973766cffacf48e6e84c7986af4d
MMostella <masonmostella@gmail.com>
2022-07-15 15:51:19 -0500

    Added information in AboutUs

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -12,6 +12,95 @@ export default function About() {
   return (
     <StyledAbout>
       <h1>About Us</h1>
+      <div>
+        <p>
+          In today's ever changing economic environment business owners must be
+          aware of one thing, bottom line profits. Whether your business is a
+          Service Business or a Retail Business we understand your desire for
+          additional cash flow and income streams. So, how can we help?
+        </p>
+      </div>
+      <div>
+        <p>
+          Created by innovative business owners who understand your business,
+          NutraBiz LLC is on the cutting edge of futurewave marketing. We
+          believe the way products and services will be used in the future is
+          about to change. So, why not adapt and change with the future? You can
+          now benefit from our highly unique B2B Niche Marketing System and our
+          desire to help your business reach the next level in this future
+          economy. In order to accomplish this, certain business trends must be
+          embraced and faithfully, yet happily, executed. What are those trends
+          and how can your business capitalize?
+        </p>
+      </div>
+      <div>
+        <h2>Service Business</h2>
+        <p>
+          Lets start with Service businesses and what the future, with NutraBiz
+          LLC as your partner, will look like. First, Service businesses are
+          very labor intensive and takes a toll on every part of your life. If
+          you are a Salon Owner or Chiropractor you are paid for your labor. If
+          you are sick and cant work you dont get paid unless you have
+          associates working under you that you supervise, and they probably
+          cant pay your overhead without you being there. Did you find that to
+          be true during the pandemic? You have job security because services
+          can not be done on the internet so you will always have foot traffic
+          in your front door. Now, here is the shift! What if there was a way
+          for you to grow your foot traffic, build goodwill in the community by
+          providing a new service, and potentially double your bottom line
+          profits with a self funding addition. That will not only pay for
+          itself quickly but provide you with added revenue for years to come?
+          Your business can become a Stay N Shape Weight Loss Center and
+          re-cashflow your foot traffic! Think about the possibilities when
+          everybody is getting energy, losing pounds and inches, feeling
+          motivated with great mental clarity and they have to come into YOUR
+          Business to get it. Think about the referrals you will get from
+          existing foot traffic. When those referrals are in your business
+          purchasing products they will book additional services also. We want
+          to make it easier. For every bottle you purchase we give you 2 free
+          sample packs and 2 free brochures to assure you sell your bottles
+          quickly, all while doubling your money over and over. Gyms owners can
+          benefit the exact same way as both a Service business and a Retail
+          business! Stay N Shape is a testimony rich product with over 30
+          Million capsules of the formula already sold worldwide. This product
+          combined with our niche marketing system is a no-brainer for the
+          Service business industry. Plus you can not fail. Any unsold product
+          you may have after 6 months of your initial order we will gladly take
+          back and pay you 100% of what you paid for the unopened product. We
+          exist to bless your business.
+        </p>
+      </div>
+      <div>
+        <h2>Retail Business</h2>
+        <p>
+          For our Retail business friends. As you know, the internet has really
+          hurt the Retail business, as well as, the Mom and Pop brick and mortar
+          stores. If your Retail business is a Health Food Store, Herb Shop,
+          Gym, Fitness Center or Drug Store, the supplements you carry can most
+          likely be bought cheaper on the internet than from your business.
+          Those brand names your business carries are widely available across
+          the internet at discount prices. So, how can we help your business?
+          Enter to become a Stay N Shape Weight Loss Center. GNC, Whole Foods,
+          Vitamin World, CVS and Walgreens are not Stay N Shape Weight Loss
+          Centers. As a matter of fact, they are not weight loss centers at all.
+          There is your New claim to fame. You are now a Stay N Shape Weight
+          Loss Center and can advertise your Retail business as such. There is a
+          New Buzz in the community about your business! And, we dont compete
+          with your business by selling products, at the same price or cheaper,
+          on the internet. Customers and their referrals have to physically walk
+          into your business to get it for cheaper. And we know 90% of the time
+          they will grab other products while there. We provide all businesses
+          with window signs and also have Stay N Shape Weight Loss Center
+          banners available as well. We also make it easier. We give you 2
+          sample packs and 2 brochures for every bottle purchased. You can also
+          offer your customers a guarantee which will make Stay N Shape stand
+          out above those Name Brands. If a customer brings a bottle back saying
+          it didnt work, simply refund their money and send us the bottle with a
+          copy of their drivers license. We will replace the bottle, no
+          questions asked. As you can see, we know Retail. Become a Stay N Shape
+          Weight Loss Center today and outsmart the competition!
+        </p>
+      </div>
     </StyledAbout>
   );
 }
